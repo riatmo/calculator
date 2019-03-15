b2fb917f234c4f5163ecd63e628b76dca514a2d8
Ragil Triatmojo <ragil@redpedia.com>
2019-03-15 21:51:50 +0700

    Menambah blok program kalkulator : input, menu, operasi & percabangan

---
+++ calculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CaclulatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input nilai
+            Console.WriteLine("Masukan angka pertama : ");
+            float a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Masukan angka kedua : ");
+            float b = Convert.ToInt32(Console.ReadLine());
+
+            //Opsi kalkulator
+            Console.WriteLine();
+            Console.WriteLine("Pilihan operasi hitung kalkulator  ");
+            Console.WriteLine("_________________________________________");
+            Console.WriteLine("1: Perkalian");
+            Console.WriteLine("2: Penjumlahan");
+            Console.WriteLine("3: Pengurangan");
+            Console.WriteLine("4: Pembagian");
+            Console.WriteLine("__________________________________________");
+
+            Console.WriteLine("Masukan pilihan perhitungan : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            //Percabangan & eksekusi nilai
+            if (choice == 1)
+            {
+                Console.WriteLine("Hasil : " + (a * b));
+                Console.Read();
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("Hasil : " + (a + b));
+
+                Console.Read();
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Hasil : " + (a - b));
+
+                Console.Read();
+            }
+            else if (choice == 4)
+            {
+                Console.WriteLine("Hasil : " + (a / b));
+
+                Console.Read();
+            }
+
+            else
+            {
+
+                Console.WriteLine("Pastikan memilih antara menu 1-4");
+
+            }
+
+        }
+    }
+}
